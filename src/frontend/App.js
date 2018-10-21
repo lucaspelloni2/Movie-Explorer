11f81b94be2bb599d59d4769e335705ca7c7d1be
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-21 12:28:00 +0200

    body gradient

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { BACKGROUND_COLOR } from "./helpers/colors";
 
-const Container = styled.div`
-  background-color: ${BACKGROUND_COLOR};
-`;
+const Container = styled.div``;
 
 class App extends Component {
   render() {
     return (
       <Container>
         <h1>My Title</h1>
+        <h1>My Title</h1>
+
         <h3>My SubTitle</h3>
         <p>
           This is my paragraph aasf as fa sfas fas fas fasff asfas f
