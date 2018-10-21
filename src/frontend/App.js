44d459c61fb9d8fb8a20fd68ac41f02ac2840db2
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-21 12:15:50 +0200

    initial react commit

---
+++ src/frontend/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { BACKGROUND_COLOR } from "./helpers/colors";
+
+const Container = styled.div`
+  background-color: ${BACKGROUND_COLOR};
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>My Title</h1>
+        <h3>My SubTitle</h3>
+        <p>
+          This is my paragraph aasf as fa sfas fas fas fasff asfas f
+          asfasfjoaskof
+        </p>
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/frontend/helpers/colors.js
@@ -0,0 +1,1 @@
+export const BACKGROUND_COLOR = "#282c34";
