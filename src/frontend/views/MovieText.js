034cf3dc6e4bdaa7e5715d17f41497b3b431e4b2
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-23 23:09:40 +0200

    padding

--- src/frontend/views/MovieText.js
+++ src/frontend/views/MovieText.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 10px;
+  padding: 0 12px;
 `;
 
 const MovieTitle = styled.h4`
