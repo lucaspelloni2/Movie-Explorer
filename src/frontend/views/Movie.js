97ff8b47a6af515f5046670315b6858de5818f96
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-10-23 22:24:09 +0200

    mask for poster

--- src/frontend/views/Movie.js
+++ src/frontend/views/Movie.js
@@ -4,12 +4,28 @@ import MoviePoster from "./MoviePoster";
 
 const Card = styled.div`
   display: flex;
+  flex-direction: column;
   margin: 20px;
+  width: 200px;
+`;
+
+const MovieTitle = styled.h4`
+  margin-top: 8px;
+  margin-bottom: 2px;
+  word-break: break-word;
+`;
+
+const MovieYear = styled.h4`
+  color: gray;
+  font-size: 12px;
+  margin-top: 0;
 `;
 const Movie = ({ movie }) => {
   return (
     <Card>
       <MoviePoster poster={movie.Poster} width={200} height={300} />
+      <MovieTitle>{movie.Title}</MovieTitle>
+      <MovieYear>{movie.Year}</MovieYear>
     </Card>
   );
 };
--- src/frontend/views/MoviePoster.js
+++ src/frontend/views/MoviePoster.js
@@ -5,6 +5,17 @@ const Image = styled.img`
   width: ${props => props.width}px;
   height: ${props => props.height}px;
   border-radius: 4px;
+  -webkit-mask-image: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0, black),
+    color-stop(0.35, black),
+    color-stop(0.5, black),
+    color-stop(0.65, black),
+    color-stop(0.85, rgba(0, 0, 0, 0.6)),
+    color-stop(1, transparent)
+  );
 `;
 
 const MoviePoster = ({ poster, ...otherProps }) => {
